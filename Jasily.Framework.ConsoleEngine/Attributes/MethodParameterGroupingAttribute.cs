c49e8f1ea27ee955a937b44a0df4ed76aa4ee219
赖巍 <skyoflw@gmail.com>
2016-02-14 16:24:35 +0800

    refactor

--- Jasily.Framework.ConsoleEngine/Attributes/MethodParameterGroupingAttribute.cs
+++ Jasily.Framework.ConsoleEngine/Attributes/MethodParameterGroupingAttribute.cs
@@ -5,9 +5,6 @@ namespace Jasily.Framework.ConsoleEngine.Attributes
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
     public sealed class MethodParameterGroupingAttribute : Attribute
     {
-        public static MethodParameterGroupingAttribute Default { get; }
-            = new MethodParameterGroupingAttribute(0);
-
         public int GroupId { get; }
 
         public MethodParameterGroupingAttribute(int groupId)
